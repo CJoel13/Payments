a79afafc25fe025f4c4300e33da56f84035055dc
Cristian Joel Gonzalez Morales <joel13gm@gmail.com>
2020-04-09 16:24:35 -0500

    Estructura inicial de Payment_O

---
+++ src/test/java/com/cris/payments/PaymentOApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cris.payments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentOApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
