cac952dccc393973bca58cb47ca545913a83e2f3
Cristian Joel Gonzalez Morales <joel13gm@gmail.com>
2020-04-10 14:33:32 -0500

    Se crea Controller para validar funcionamiento.

---
+++ src/main/java/com/cris/payments/Controller/PaymentController.java
@@ -0,0 +1,16 @@
+package com.cris.payments.Controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/controller")
+public class PaymentController {
+	
+	@PostMapping("/postRequest")
+	public String getPayment(){		
+		return "Respuesta ok";
+	}
+
+}
