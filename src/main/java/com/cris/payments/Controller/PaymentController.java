7a09a30c3712d821cae88e890fe97286f4b484f7
Cristian Joel Gonzalez Morales <joel13gm@gmail.com>
2020-04-10 14:56:11 -0500

    Se agrega segundo metodo a Controller (get)

--- src/main/java/com/cris/payments/Controller/PaymentController.java
+++ src/main/java/com/cris/payments/Controller/PaymentController.java
@@ -1,5 +1,6 @@
 package com.cris.payments.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,11 @@ public class PaymentController {
 	@PostMapping("/postRequest")
 	public String getPayment(){		
 		return "Respuesta ok";
+	}
+	
+	@GetMapping("/getRequest")
+	public String getWord() {
+		return "Ejemplo 2";
 	}
 
 }
